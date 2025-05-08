1e0f2e273f68329bbc5fef57886365d59b7ad172
zDyant <dyant@proton.me>
2025-05-08 09:02:37 -0300

    add markview plugin

---
+++ lua/custom/plugins/markview.lua
@@ -0,0 +1,9 @@
+-- https://github.com/OXY2DEV/markview.nvim#-installation
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Do not lazy load this plugin as it is already lazy-loaded.
+
+    dependencies = {
+        "saghen/blink.cmp"
+    },
+};
