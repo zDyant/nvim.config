ed36af22eacb6219e1eb3b78e3af9a1fcc04d5c5
zDyant <dyant@proton.me>
2025-08-17 10:38:52 -0300

    merge with upstream

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,29 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ea85de2 (add lazy-events plugin and config)
 -- https://github.com/bwpge/lazy-events.nvim
 vim.g.lazy_events_config = {
   simple = {
     LazyFile = { "BufReadPost", "BufNewFile", "BufWritePre" },
   }
 }
-
-<<<<<<< HEAD
-require('lazy').setup({
-  -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
-  --
-  --
-  -- Required for event = 'LazyFile'
-  { "bwpge/lazy-events.nvim", import = "lazy-events.import", lazy = false },
-  
-  -- import automatically all plugins
-  { import = 'custom.plugins' },
-  { import = 'kickstart.plugins'},
-
-=======
-=======
->>>>>>> ea85de2 (add lazy-events plugin and config)
 require('lazy').setup({
   --
   -- import automatically all plugins
@@ -33,7 +13,6 @@ require('lazy').setup({
   { import = 'custom.plugins' },
   { import = 'kickstart.plugins'},
   --
->>>>>>> 982d981 (import plugins)
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
