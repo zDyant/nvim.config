bf636807dc108d65ae6e22f07c3d660d5e00946e
zDyant <dyant@proton.me>
2025-09-15 15:51:20 -0300

    fix: twilight startup config

--- lua/custom/plugins/twilight.lua
+++ lua/custom/plugins/twilight.lua
@@ -1,10 +1,8 @@
 return {
   {
     'folke/twilight.nvim',
-    event = 'LazyFile',
     config = function()
       require('twilight').setup()
-
       -- Workarround
       vim.api.nvim_create_autocmd('VimEnter', {
         callback = function()
