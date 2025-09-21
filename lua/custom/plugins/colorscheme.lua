d984c4936cfba84902bf4037e25fd800166e8441
zDyant <dyant@proton.me>
2025-09-20 21:39:26 -0300

    update: colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,16 +1,13 @@
 return {
   -- { 'catppuccin/nvim' },
   -- { 'sontungexpt/witch' },
-  { 'rose-pine/neovim',
-      config = function()
-      -- Load the colorscheme here.
-      -- Many themes have different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'rose-pine'
-    end,
-},
+  { 'rose-pine/neovim' },
   -- { 'olivercederborg/poimandres.nvim' },
   -- { 'antonio-hickey/citrus-mist' },
-  { 'diegoulloao/neofusion.nvim' },
-
+  {
+    'diegoulloao/neofusion.nvim',
+    config = function()
+      vim.cmd.colorscheme 'neofusion'
+    end,
+  },
 }
