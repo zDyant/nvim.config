1078281d076669955dac79a832a20fae5c4936df
zDyant <dyant@proton.me>
2026-04-19 10:12:56 -0300

    update obsidian.nvim

--- lua/custom/plugins/markview.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/OXY2DEV/markview.nvim#-installation
-return {
-    "OXY2DEV/markview.nvim",
-    lazy = false, -- Do not lazy load this plugin as it is already lazy-loaded.
-
-    dependencies = {
-        "saghen/blink.cmp"
-    },
-};
--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -2,6 +2,18 @@ return {
   {
     'obsidian-nvim/obsidian.nvim',
     ft = 'markdown',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      {
+        'saghen/blink.cmp',
+        opts = {
+          fuzzy = { implementation = 'lua' }, -- no need to build binary
+        },
+      },
+      { 'OXY2DEV/markview.nvim', lazy = false },
+    },
     ---@module 'obsidian'
     ---@type obsidian.config
     opts = {
@@ -9,17 +21,22 @@ return {
       workspaces = {
         {
           name = 'default',
-          path = '~/Documents/college',
+          path = '~/Documents/obsidian',
         },
       },
-    },
+      follow_url_func = function(url)
+        vim.fn.jobstart({ 'xdg-open', url }, { detach = true })
+      end,
 
-    config = function()
-      require('obsidian').setup {
-        follow_img_func = function(img)
-          vim.fn.jobstart({ 'xdg-open', url})
+      follow_img_func = function(img)
+        vim.fn.jobstart({ 'xdg-open', img }, { detach = true })
+      end,
+
+      open = {
+        func = function(uri)
+          vim.fn.jobstart({ 'xdg-open', uri }, { detach = true })
         end,
-      }
-    end,
+      },
+    },
   },
 }
