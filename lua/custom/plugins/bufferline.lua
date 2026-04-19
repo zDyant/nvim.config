8077578f4efa89f77f8933e7ab0e5b04f70fe877
zDyant <dyant@proton.me>
2026-04-19 17:03:27 -0300

    remove unused keybind

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -9,7 +9,6 @@ return {
     keys = {
       { "<Tab>",   ":BufferLineCycleNext<CR>",  desc = "Next Buffer", silent = true },
       { "<S-Tab>", ":BufferLineCyclePrev<CR>",  desc = "Prev Buffer", silent = true },
-      { "bc",      ":BufferLinePick<CR>",       desc = "Go to selected buffer", silent = true },
       { "bl",      ":BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right", silent = true, },
       { "bh",      ":BufferLineCloseLeft<CR>",  desc = "Delete Buffers to the Left", silent = true },
       { "bc",      ":BufferLinePickClose<CR>",  desc = "Delete selected buffer", silent = true }
