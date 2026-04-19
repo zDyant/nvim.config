5127d3058954bc4f5d177cb12444471e216606e7
zDyant <dyant@proton.me>
2026-04-19 11:21:55 -0300

    update noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -5,21 +5,22 @@ return {
     opts = {},
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+      'hrsh7th/nvim-cmp',
     },
     presets = { lsp_doc_border = true },
     config = function()
-      require('noice').setup({
+      require('noice').setup {
         lsp = {
           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
           },
         },
-      })
+      }
     end,
-  }
+  },
 }
