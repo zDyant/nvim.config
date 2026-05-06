c609ace6658dbacafcbe89254ccf081ce84cdfea
zDyant <dyant@proton.me>
2026-05-06 00:26:04 -0300

    add vim.o.winborder

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.o.winborder = 'single'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
