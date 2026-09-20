4207bfed78d236d63254863f62ae5fcad98f1e9f
zDyant <dyant@proton.me>
2026-09-20 20:05:46 -0300

    chore: disable unused LuaRocks support

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,8 +1,9 @@
 require('lazy').setup({
-  --
   { import = 'custom.plugins' },
-  { import = 'kickstart.plugins'},
-  --
+  { import = 'kickstart.plugins' },
+}, {
+  rocks = { enabled = false },
+
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
