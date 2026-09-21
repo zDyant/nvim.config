b8974df48fcec996f312b3f8d6f40c67bf9141d9
zDyant <dyant@proton.me>
2026-09-20 22:08:44 -0300

    fix(lsp): preserve lua_ls settings during initialization

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -137,7 +137,8 @@ return {
               end
             end
 
-            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            local current_settings = client.config.settings --[[@as lspconfig.settings.lua_ls]]
+            client.config.settings.Lua = vim.tbl_deep_extend('force', current_settings.Lua, {
               runtime = {
                 version = 'LuaJIT',
                 path = { 'lua/?.lua', 'lua/?/init.lua' },
