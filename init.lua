3dbc070bea395e6e7e4f04b8bcc8515797044b29
zDyant <dyant@proton.me>
2026-09-20 22:06:50 -0300

    chore: enable the Neovim Lua loader early

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 --  NOTE: https://www.youtube.com/watch?v=m8C0Cq9Uv9o
 --  NOTE: If you need any help
 --
+-- Enable faster startup by caching compiled Lua modules
+vim.loader.enable()
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
